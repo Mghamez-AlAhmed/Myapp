df005116c9fac95fbb75d101c45113146b83de51
Mghamez-AlAhmed <Mghamez_Al-Ahmed@outlook.com>
2024-09-10 18:23:43 -0400

    commit1

--- lib/Api_Links/api_links.dart
+++
@@ -1,4 +0,0 @@
-String  baseUrl="https://tarmeezacademy.com/api/v1/register";
-String baseUrll="https://tarmeezacademy.com/api/v1/login";
-String showpostlinks="https://tarmeezacademy.com/api/v1/posts/1";
-String mghamez="I am Mghamez an application programmer specializing in Flutter I create cross platform mobile apps that are both efficient and user friendly With strong skills in  front end  development I am eager to contribute to your company by building high quality applications and collaborating on innovative projects";
